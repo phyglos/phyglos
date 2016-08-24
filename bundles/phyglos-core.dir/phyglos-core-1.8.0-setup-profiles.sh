88f03282e3602c0a8e5979bbf2a23b088f81f22b
alz-phyglos <alz@phyglos.org>
2016-08-24 05:40:33 +0200

    Set MANPATH in profile.d/extrapaths.sh

--- bundles/phyglos-core.dir/phyglos-core-1.8.0-setup-profiles.sh
+++ bundles/phyglos-core.dir/phyglos-core-1.8.0-setup-profiles.sh
@@ -149,25 +149,30 @@ fi
 EOF
 
      cat > /etc/profile.d/extrapaths.sh << "EOF"
+# Set PCK_CONFIG_PATH
 if [ -d /usr/local/lib/pkgconfig ] ; then
     pathappend /usr/local/lib/pkgconfig PKG_CONFIG_PATH
 fi
 
+# Set PATH
 if [ -d /usr/local/bin ]; then
     pathprepend /usr/local/bin
 fi
 if [ -d /usr/local/sbin -a $EUID -eq 0 ]; then
     pathprepend /usr/local/sbin
 fi
-
 if [ -d ~/bin ]; then
     pathprepend ~/bin
 fi
-
-# Set current dir in PATH
 if [ $EUID -gt 99 ]; then
     pathappend .
 fi
+
+# Set MANPATH
+pathappend /usr/share/man  MANPATH
+
+# Set MANPATH
+pathappend /usr/share/info INFOPATH
 EOF
 
      cat > /etc/profile.d/i18n.sh << EOF
