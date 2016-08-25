136f204c98c466f0db2263510455fd05717209fc
alz-phyglos <alz@phyglos.org>
2016-08-25 06:55:46 +0200

    Added lsof_4.89 to phyglos-syscore

---
+++ bundles/phyglos-syscore.dir/lsof_4.89.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+build_compile()
+{
+    #Unpack embedded tarball
+    tar -xf lsof_4.89_src.tar
+    cd lsof_4.89_src           
+
+    ./Configure -n linux 
+
+    make CFGL="-L./lib -ltirpc"
+}
+
+build_pack()
+{
+    bandit_mkdir $BUILD_PACK/usr/bin
+    install -v -m0755 -o root -g root lsof $BUILD_PACK/usr/bin
+
+    bandit_mkdir $BUILD_PACK/usr/share/man/man8
+    install -v lsof.8 $BUILD_PACK/usr/share/man/man8
+}
