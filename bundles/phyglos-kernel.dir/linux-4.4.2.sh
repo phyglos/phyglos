c617b5aef91925ac80f096c4e562ab596f8e6e1a
alz-phyglos <alz@phyglos.org>
2016-08-31 16:36:12 +0200

    Save a copy of the kernel sources

--- bundles/phyglos-kernel.dir/linux-4.4.2.sh
+++ bundles/phyglos-kernel.dir/linux-4.4.2.sh
@@ -38,6 +38,7 @@ build_compile()
 build_pack()
 {
     # Pack the kernel
+    bandit_log "Installing the kernel..."
     bandit_mkdir $BUILD_PACK/boot
     cp -v arch/$PHY_KERNEL_ARCH/boot/bzImage \
 	             $BUILD_PACK/boot/vmlinuz-$PHY_KERNEL_VER-$PHY_KERNEL_ARCH-$PHY_KERNEL_HW
@@ -45,6 +46,7 @@ build_pack()
     cp -v .config    $BUILD_PACK/boot/$PHY_KERNEL_CFG.config
 
     # Pack kernel modules and firmware
+    bandit_log "Installing modules..."
     bandit_mkdir $BUILD_PACK/lib/modules
     bandit_mkdir $BUILD_PACK/lib/firmware
     make INSTALL_MOD_PATH="$BUILD_PACK" modules_install
@@ -52,26 +54,42 @@ build_pack()
     # Configure USB modules
     bandit_mkdir $BUILD_PACK/etc
     install -v -m755 -d $BUILD_PACK/etc/modprobe.d
-    cat > $BUILD_PACK/etc/modprobe.d/usb.conf <<EOF
+    cat > $BUILD_PACK/etc/modprobe.d/usb.conf << EOF
 # Load USB modules in correct order
 install ohci_hcd /sbin/modprobe ehci_hcd ; /sbin/modprobe -i ohci_hcd ; true
 install uhci_hcd /sbin/modprobe ehci_hcd ; /sbin/modprobe -i uhci_hcd ; true
 EOF
 
-    # Pack documentation
-    bandit_mkdir $BUILD_PACK/usr/share/doc/$PHY_KERNEL_SRC
-    cp -r Documentation/* $BUILD_PACK/usr/share/doc/$PHY_KERNEL_SRC
-
     # Pack a copy of the kernel build directory 
     if [ $PHY_KERNEL_KEEP_BUILD = "yes" ]; then
-	rm -f $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/build
-	rm -f $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/source
-	rm -rf Documentation
+	bandit_log "Saving build tree..."
+	rm -rf $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/build
 
 	bandit_mkdir $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/build
 	cp -R * $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/build
-	
-	ln -vs /lib/modules/$PHY_KERNEL_VER/build $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/source
+
+	# Make a link from build to sources if they are not saved later
+	if [ $PHY_KERNEL_KEEP_SOURCE != "yes" ]; then
+	    rm -rf $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/source
+	    ln -s /lib/modules/$PHY_KERNEL_VER/build $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/source
+	fi
     fi
+    
+    # Pack a clean copy of the kernel source directory 
+    if [ $PHY_KERNEL_KEEP_SOURCE = "yes" ]; then
+	# Clean source tree again
+	bandit_log "Cleaning source tree..."
+	rm -rf $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/source
+	
+	make clean
+	bandit_mkdir $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/source
+	cp -R * $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/source	
+    fi
+
+    # Pack documentation
+    bandit_mkdir $BUILD_PACK/usr/share/doc/$PHY_KERNEL_SRC
+    cp -r Documentation/* $BUILD_PACK/usr/share/doc/$PHY_KERNEL_SRC
+    rm -rf $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/source/Documentation
+    rm -rf $BUILD_PACK/lib/modules/$PHY_KERNEL_VER/build/Documentation
 }
 
