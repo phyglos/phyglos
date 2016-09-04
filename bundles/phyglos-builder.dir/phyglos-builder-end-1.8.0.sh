45177d3965ce1d313c606e421aea8043f1b5cb84
alz-phyglos <alz@phyglos.org>
2016-09-04 06:23:17 +0200

    Renaming of phyglos scripts

--- bundles/phyglos-builder.dir/phyglos-builder-end-1.8.0.sh
+++ bundles/phyglos-builder.dir/phyglos-builder-end-1.8.0.sh
@@ -3,8 +3,8 @@
 script_run()
 {
     # Strip debuggins symbols
-    strip --strip-debug $BANDIT_BUILDER_DIR/lib/*
-    /usr/bin/strip --strip-unneeded $BANDIT_BUILDER_DIR/{,s}bin/*
+    strip --strip-debug $BANDIT_BUILDER_DIR/lib/* 2>&1
+    /usr/bin/strip --strip-unneeded $BANDIT_BUILDER_DIR/{,s}bin/* 2>&1
 
     # Remove unnecessary documentation
     rm -rf $BANDIT_BUILDER_DIR/{,share}/{info,man,doc}
