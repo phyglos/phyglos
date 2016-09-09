b2cae07f4d6d939b7e299309f00f185adb02ee99
alz-phyglos <alz@phyglos.org>
2016-09-09 07:11:31 +0200

    Patch and pack BLFS bootscripts

--- bundles/phyglos-core.dir/blfs-bootscripts-20150924.sh
+++ bundles/phyglos-core.dir/blfs-bootscripts-20150924.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+build_compile()
+{
+    bandit_log "Patching init scripts..."
+    
+    ## D-Bus
+    # Patch service messages
+    sed -e"s/D-Bus Messagebus/D-Bus/g" \
+	-i blfs/init.d/dbus
+    
+    ## NTP  
+    # Patch daemon user name
+    sed -e"s/ntp:ntp/ntpd:ntp/g" \
+	-i blfs/init.d/ntpd
+    # Patch service name 
+    sed -e"s/ing ntp/ing Network Time Protocol/g" \
+	-i blfs/init.d/ntpd
+
+    ## Remove the template file  
+    rm -rf blfs/init.d/template
+}
+
 build_pack()
 {
-    bandit_mkdir $BANDIT_HOME/lib/blfs-bootscripts
-    cp -vR * $BANDIT_HOME/lib/blfs-bootscripts
+    bandit_mkdir $BUILD_PACK/lib/blfs-bootscripts
+    cp -vR * $BUILD_PACK/lib/blfs-bootscripts
 }
 
