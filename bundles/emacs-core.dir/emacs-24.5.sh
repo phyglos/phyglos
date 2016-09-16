a5bb88eb840a3d85f008721fe03644c1e6d12e0d
alz-phyglos <alz@phyglos.org>
2016-09-16 05:22:22 +0000

    Added /etc/profile.d/emacs.sh to emacs-core bundle

--- bundles/emacs-core.dir/emacs-24.5.sh
+++ bundles/emacs-core.dir/emacs-24.5.sh
@@ -13,5 +13,10 @@ build_pack()
 {
     make DESTDIR=$BUILD_PACK install
     chown -R root:root $BUILD_PACK/usr/share/emacs/24.5
+
+    bandit_mkdir $BUILD_PACK/etc/profile.d
+    cat > $BUILD_PACK/etc/profile.d/emacs.sh <<EOF
+export EDITOR=emacs
+EOF
 }
 
