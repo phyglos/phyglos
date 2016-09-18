307531c5763db7ff3866c83b78837fc67d3114f4
alz-phyglos <alz@phyglos.org>
2016-09-18 04:30:54 +0000

    Added xarchiver-0.5.4 to x11-deck

---
+++ bundles/x11-deck.dir/xarchiver-0.5.4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+build_compile()
+{
+    patch -Np1 -i $BUILD_SOURCES/xarchiver-0.5.4-fixes-1.patch
+
+    ./autogen.sh \
+	--prefix=/usr               \
+        --libexecdir=/usr/lib/xfce4 \
+        --disable-gtk3              \
+        --docdir=/usr/share/doc/xarchiver-0.5.4
+    
+    make
+}
+
+build_pack()
+{
+    make DESTDIR=$BUILD_PACK \
+         DOCDIR=/usr/share/doc/xarchiver-0.5.4 \
+	 install   
+}
