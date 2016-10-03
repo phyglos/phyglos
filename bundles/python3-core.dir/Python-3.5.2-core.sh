94a8b2b5d3cf14ba3534e924467deba67102253b
alz-phyglos <alz@phyglos.org>
2016-10-03 06:35:57 +0200

    Added python3-core bundle

---
+++ bundles/python3-core.dir/Python-3.5.2-core.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+build_compile()
+{
+    CXX="/usr/bin/g++"      \
+    ./configure             \
+	--prefix=/usr       \
+	--enable-shared     \
+	--with-system-expat \
+	--with-system-ffi   \
+	--without-ensurepip
+    
+    make
+}
+
+build_pack()
+{
+    make DESTDIR=$BUILD_PACK install
+
+    chmod -v 755 $BUILD_PACK/usr/lib/libpython3.5m.so
+    chmod -v 755 $BUILD_PACK/usr/lib/libpython3.so
+}
+
