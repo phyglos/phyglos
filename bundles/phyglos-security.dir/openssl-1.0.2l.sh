c1e820364fb6a7e1fc4c5be9b627cba37a06b035
Alz <alz@phyglos.org>
2017-07-01 11:20:51 +0200

    Install openssl-1.0.2l in separate directories

--- bundles/phyglos-security.dir/openssl-1.0.2l.sh
+++ bundles/phyglos-security.dir/openssl-1.0.2l.sh
@@ -5,19 +5,21 @@ build_compile()
     # Always build with an empty CCache
     bandit_system_has ccache && ccache -C
 
+    patch -Np1 -i $BUILD_SOURCES/openssl-1.0.2l-compat_versioned_symbols-1.patch
+    
     # Remove unneeded encryptation and compression methods
-    ./config                  \
-	--prefix=/usr         \
-        --openssldir=/etc/ssl \
-        --libdir=lib          \
-        shared                \
-        no-idea               \
-	no-rc5                \
-	no-psk                \
-	no-ssl2               \
-	no-ssl3               \
-	no-weak-ssl-ciphers   \
-        no-comp               \
+    ./config                     \
+        --prefix=/usr            \
+        --openssldir=/etc/ssl    \
+        --libdir=lib/openssl-1.0 \
+        shared                   \
+        no-idea                  \
+        no-rc5                   \
+        no-psk                   \
+        no-ssl2                  \
+        no-ssl3                  \
+        no-weak-ssl-ciphers      \
+        no-comp                  \
         no-zlib
 
     make depend
@@ -32,14 +34,16 @@ build_test()
 
 build_pack()
 {
-    # Avoid installing static libraries
-    sed -i 's# libcrypto.a##;s# libssl.a##' Makefile
-
     make INSTALL_PREFIX=$BUILD_PACK \
 	MANDIR=/usr/share/man \
 	MANSUFFIX=ssl \
-	install 
+	install_sw
 
-    install -dv -m755 $BUILD_PACK/usr/share/doc/openssl-1.0.2l
-    cp -vfr doc/*     $BUILD_PACK/usr/share/doc/openssl-1.0.2l
+    mv -v  $BUILD_PACK/usr/lib/openssl-1.0/lib{crypto,ssl}.so.1.0.0 $BUILD_PACK/usr/lib 
+    ln -sv ../libssl.so.1.0.0         $BUILD_PACK/usr/lib/openssl-1.0        
+    ln -sv ../libcrypto.so.1.0.0      $BUILD_PACK/usr/lib/openssl-1.0        
+
+    mv -v $BUILD_PACK/usr/include/openssl $BUILD_PACK/usr/include/openssl-1.0
+    
+    sed 's@/include$@/include/openssl-1.0@' -i $BUILD_PACK/usr/lib/openssl-1.0/pkgconfig/*.pc
 }
