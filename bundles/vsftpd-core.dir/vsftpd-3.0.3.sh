f28309175163e53d7ea74326ea0f4484877f7172
Alz <alz@phyglos.org>
2017-08-23 09:30:01 +0200

    Install init script in vsftpd-3.0.3

--- bundles/vsftpd-core.dir/vsftpd-3.0.3.sh
+++ bundles/vsftpd-core.dir/vsftpd-3.0.3.sh
@@ -52,13 +52,83 @@ auth       include      system-auth
 account    include      system-account
 session    include      system-session
 EOF
+
+    # Add init script
+    bandit_mkdir $BUILD_PACK/etc/init.d
+    cat > $BUILD_PACK/etc/init.d/vsftpd <<"EOF"
+#!/bin/bash
+#
+# Copyright (C) 2017 Angel Linares Zapater
+# Based on the work of the Linux from Scratch project
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as 
+# published by the Free Software Foundation. See the COPYING file.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the 
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+#
+### BEGIN INIT INFO
+# Provides:            ftpd
+# Required-Start:      $remote_fs
+# Should-Start:
+# Required-Stop:       $remote_fs
+# Should-Stop:
+# Default-Start:       3 4 5
+# Default-Stop:        0 1 2 6
+# Short-Description:   vsftpd daemon
+# Description:         Very Secure FTP Daemon
+### END INIT INFO
+
+. /lib/lsb/init-functions
+
+case "$1" in
+   start)
+      log_info_msg "Starting vsFTP Server..."
+      start_daemon /usr/sbin/vsftpd
+      evaluate_retval
+      ;;
+   stop)
+      log_info_msg "Stopping vsFTP Server..."
+      killproc /usr/sbin/vsftpd
+      evaluate_retval
+      ;;
+   reload)
+      log_info_msg "Reloading vsFTP Server..."
+      killproc /usr/sbin/vsftpd -HUP
+      evaluate_retval
+      ;;
+   restart)
+      $0 stop
+      sleep 1
+      $0 start
+      ;;
+   status)
+      statusproc /usr/sbin/vsftpd
+      ;;
+   *)
+      echo "Usage: $0 {start|stop|reload|restart|status}"
+      exit 1
+      ;;
+esac
+EOF
+    chmod 754 $BUILD_PACK/etc/init.d/vsftpd
+    for i in 3 4 5; do
+	bandit_mkdir $BUILD_PACK/etc/rc.d/rc$i.d
+	ln -svf ../init.d/vsftpd $BUILD_PACK/etc/rc.d/rc$i.d/S32vsftpd
+    done
+    for i in 0 1 2 6; do
+	bandit_mkdir $BUILD_PACK/etc/rc.d/rc$i.d
+	ln -svf ../init.d/vsftpd $BUILD_PACK/etc/rc.d/rc$i.d/K28vsftpd
+    done
 }
 
 install_setup()
 {
-    # Start the service
-    pushd $BANDIT_HOME/lib/blfs-bootscripts
-      make install-vsftpd
-      /etc/init.d/vsftpd start
-    popd
+    /etc/init.d/vsftpd start
+}
+
+remove_setup()
+{
+    /etc/init.d/vsftpd stop
 }
