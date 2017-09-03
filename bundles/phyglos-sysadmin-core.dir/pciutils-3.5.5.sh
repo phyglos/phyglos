e586e9f5ea7a9752a742d541195443fa42321d64
Alz <alz@phyglos.org>
2017-09-03 11:37:33 +0200

    Save .ids files in /usr/share/hwdata directory for pciutils-3.5.5

--- bundles/phyglos-sysadmin-core.dir/pciutils-3.5.5.sh
+++ bundles/phyglos-sysadmin-core.dir/pciutils-3.5.5.sh
@@ -2,20 +2,20 @@
 
 build_compile()
 {
-    make                         \
-	PREFIX=/usr              \
-	MANDIR=/usr/share/man    \
-	SHAREDIR=/usr/share/misc \
+    make                           \
+	PREFIX=/usr                \
+	MANDIR=/usr/share/man      \
+	SHAREDIR=/usr/share/hwdata \
 	SHARED=yes
 }
 
 build_pack()
 {
-    make                                    \
-	PREFIX=$BUILD_PACK/usr              \
-	MANDIR=$BUILD_PACK/usr/share/man    \
-	SHAREDIR=$BUILD_PACK/usr/share/misc \
-	SHARED=yes                          \
+    make                                      \
+	PREFIX=$BUILD_PACK/usr                \
+	MANDIR=$BUILD_PACK/usr/share/man      \
+	SHAREDIR=$BUILD_PACK/usr/share/hwdata \
+	SHARED=yes                            \
 	install install-lib      
 
     chmod -v 755 $BUILD_PACK/usr/lib/libpci.so
