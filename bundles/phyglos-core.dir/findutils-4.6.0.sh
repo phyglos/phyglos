a49ea139c76e165d2f7631228b7a3cdd6f062f0e
Alz <alz@phyglos.org>
2017-09-27 05:44:18 +0200

    Set proper environent in order to limit updatedb searches

--- bundles/phyglos-core.dir/findutils-4.6.0.sh
+++ bundles/phyglos-core.dir/findutils-4.6.0.sh
@@ -19,8 +19,16 @@ build_pack()
 {
     make DESTDIR=$BUILD_PACK install
 
+    # Move 'find' to /bin due to LFS-scripts dependencies
     bandit_mkdir $BUILD_PACK/bin
     mv -v $BUILD_PACK/usr/bin/find $BUILD_PACK/bin
-
     sed -i 's|find:=${BINDIR}|find:=/bin|' $BUILD_PACK/usr/bin/updatedb
+
+    # Set environment PRUNE vars to limit some searches for updatedb script
+    cat $BUILD_PACK/etc/profile.d/updatebd.sh <<"EOF" 
+export PRUNE_BIND_MOUNTS="1"
+export PRUNENAMES=".bzr .git .hg .svn"
+export PRUNEPATHS="/media /mnt /tmp /usr/tmp /var/spool /var/tmp"
+export PRUNEFS="afs autofs binfmt_misc cifs coda devfs devpts ftpfs fuse.glusterfs fuse.sshfs ecryptfs fusesmb devtmpfs lustre_lite mfs ncpfs NFS nfs nfs4 proc rpc_pipefs shfsiso9660 smbfs sysfs tmpfs usbfs udf"
+EOF
 }
