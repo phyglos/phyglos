ebb9a94def2807ea06f4a3e183cea145e3b2955e
Alz <alz@phyglos.org>
2017-10-21 10:41:07 +0200

    Fix CUPS group and remove dirs when removing

--- bundles/phyglos-printing.dir/phyglos-printing-setup.sh
+++ bundles/phyglos-printing.dir/phyglos-printing-setup.sh
@@ -2,8 +2,15 @@
 
 script_run()
 {
+    # Create group for CUPS daemon
+    groupadd -g 9 lp
+
     # Create CUPS daemon 'lp'
-    useradd -c "Print Service User" -d /var/spool/cups -g lp -s /bin/false -u 9 lp
+    useradd -c "CUPS daemon" \
+	    -d /var/spool/cups \
+	    -s /bin/false \
+	    -g lp \
+	    -u 9 lp
 
     # Create group for printing users 'lpadmin'
     groupadd -g 19 lpadmin
@@ -17,7 +24,18 @@ script_reverse()
     # Remove user phy from lpadmin group
     gpasswd -d phy lpadmin
 
-    # Delete CUPS user and group
+    # Delete CUPS user and groups
     userdel lp
+    rm -rf /var/mail/lp   
     groupdel lpadmin
+    groupdel lp
+
+    # Delete CUPS work directories
+    rm -rf /etc/cups
+    rm -rf /usr/lib/cups
+    rm -rf /usr/share/cups
+    rm -rf /run/cups
+    rm -rf /var/cache/cups
+    rm -rf /var/log/cups
+    rm -rf /var/spool/cups
 }
