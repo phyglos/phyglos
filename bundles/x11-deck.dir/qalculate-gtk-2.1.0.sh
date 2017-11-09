7777196e3357f1a07f4e964c1918cc000fc0e937
Alz <alz@phyglos.org>
2017-11-09 06:50:53 +0100

    Added qalculate-gtk-2.1.0 to x11-deck bundle

---
+++ bundles/x11-deck.dir/qalculate-gtk-2.1.0.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+build_compile()
+{   
+    ./configure           \
+	--prefix=/usr     \
+	--disable-static
+    
+    make
+}
+
+build_pack()
+{
+    make DESTDIR=$BUILD_PACK install
+}
+
+install_setup()
+{
+    gtk-update-icon-cache
+}
+
+remove_setup()
+{
+    gtk-update-icon-cache
+}
