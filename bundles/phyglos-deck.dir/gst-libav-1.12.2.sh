a6dc0268055258a9d65d01660c4ed19d6c865359
Alz <alz@phyglos.org>
2017-12-03 06:54:49 +0100

    Added gst-libav-1.12.2 to phyglos-deck bundle

---
+++ bundles/phyglos-deck.dir/gst-libav-1.12.2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+build_compile()
+{  
+    ./configure       \
+	--prefix=/usr \
+	--with-package-name="GStreamer Libav Plugins 1.12.2" \
+	--with-package-origin="http://www.phyglos.org"
+
+    make
+}
+
+build_test_level=4
+build_test()
+{
+    make check
+}
+
+build_pack()
+{
+     make DESTDIR=$BUILD_PACK install
+}
+
