a68cc18c9a4e7ece7a63029cdc95e40a88081617
Alz <alz@phyglos.org>
2017-12-05 05:15:30 +0100

    Added tango-icon-theme-0.8.90 to x11-deck bundle

--- bundles/x11-deck.dir/x11-deck-setup.sh
+++ bundles/x11-deck.dir/x11-deck-setup.sh
@@ -10,4 +10,7 @@ script_run()
     gtk-update-icon-cache -qf /usr/share/icons/Adwaita
     gtk-update-icon-cache -qf /usr/share/icons/gnome
     gtk-update-icon-cache -qf /usr/share/icons/nuoveXT2
+    gtk-update-icon-cache -qf /usr/share/icons/Tango
+
+    gdk-pixbuf-query-loaders --update-cache
 }
