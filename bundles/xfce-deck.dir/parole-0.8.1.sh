c080e2e2c6656a6162d9c72445b84b1dd18d7ba5
Alz <alz@phyglos.org>
2017-12-07 04:51:40 +0100

    Added parole-0.8.1 to xfce-deck bundle

---
+++ bundles/xfce-deck.dir/parole-0.8.1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+build_compile()
+{
+    ./configure       \
+	--prefix=/usr \
+	--enable-libnotify
+
+    make
+}
+
+build_pack()
+{
+     make DESTDIR=$BUILD_PACK install
+}
+
+install_setup()
+{
+    update-desktop-database
+    
+    gtk-update-icon-cache -t -f --include-image-data /usr/share/icons/hicolor
+}
+
