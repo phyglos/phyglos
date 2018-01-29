3d42df4643711d9313199787fafe11bd4d4c4018
Alz <alz@phyglos.org>
2018-01-29 05:38:58 +0100

    Add a second nameserver in resolv.conf if defined

--- bundles/phyglos-core.dir/phyglos-core-setup-network.sh
+++ bundles/phyglos-core.dir/phyglos-core-setup-network.sh
@@ -2,10 +2,9 @@
 
 script_run()
 {
+    bandit_log "Setting up network configuration files..."
 
-bandit_log "Setting up network configuration files..."
-
-cat > /etc/sysconfig/ifconfig.eth0 << EOF
+    cat > /etc/sysconfig/ifconfig.eth0 << EOF
 ONBOOT=yes
 IFACE=eth0
 SERVICE=ipv4-static
@@ -15,18 +14,22 @@ GATEWAY=$PHY_LAN_IP4_GATEWAY
 BROADCAST=$PHY_LAN_IP4_BROADCAST
 EOF
 
-cat > /etc/hostname << EOF
+    cat > /etc/hostname << EOF
 $PHY_HOSTNAME
 EOF
 
-cat > /etc/hosts << EOF
+    cat > /etc/hosts << EOF
 127.0.0.1 localhost
 $PHY_LAN_IP4_ADDRESS $PHY_HOSTNAME.$PHY_DOMAINNAME
 EOF
 
-cat > /etc/resolv.conf << EOF
+    cat > /etc/resolv.conf << EOF
 domain $PHY_DOMAINNAME
-nameserver $PHY_LAN_IP4_DNS
+nameserver $PHY_LAN_IP4_DNS1
 EOF
-
+    if [ -n "$PHY_LAN_IP4_DNS2" ]; then
+	cat >> /etc/resolv.conf << EOF
+nameserver $PHY_LAN_IP4_DNS2
+EOF
+    if
 }
