6ee73579a1af564e9ef50230f81f97c5a3a67117
Alz <alz@phyglos.org>
2018-04-01 11:51:27 +0200

    Added additional Ctrl-Left and Ctrl-Right key bindings

--- bundles/phyglos-core.dir/phyglos-core-setup-etc.sh
+++ bundles/phyglos-core.dir/phyglos-core-setup-etc.sh
@@ -88,6 +88,13 @@ set bell-style none
 "\eOd": backward-word
 "\eOc": forward-word
 
+"\e[1;5C": forward-word
+"\e[1;5D": backward-word
+"\e[5C": forward-word
+"\e[5D": backward-word
+"\e\e[C": forward-word
+"\e\e[D": backward-word
+
 # for linux console
 "\e[1~": beginning-of-line
 "\e[4~": end-of-line
