685d62f7dedce5492fa8f5d763f70b4bb79f59f5
Alz <alz@phyglos.org>
2018-08-07 06:47:10 +0200

    Workaround iso-codes dependency on Python3
    See http://github.com/phyglos/phyglos/issues/2

---
+++ bundles/python3-deck.dir/iso-codes-3.79.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+build_compile()
+{
+    ./configure --prefix=/usr
+
+    make
+}
+
+build_test_level=4
+build_test()
+{
+    make check 
+}
+
+build_pack()
+{
+    make DESTDIR=$BUILD_PACK install
+}
