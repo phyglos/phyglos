2425b2806acba81f2efa45de5fb0d93640d10e91
Alz <alz@phyglos.org>
2018-08-23 07:09:40 +0200

    Added nodejs-core bundle

---
+++ bundles/nodejs-core.dir/c-ares-1.14.0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ 
+build_compile()
+{
+    ./configure           \
+	--prefix=/usr     \
+	--disable-static
+
+    make
+}
+
+build_pack()
+{
+    make DESTDIR=$BUILD_PACK install
+}
---
+++ bundles/nodejs-core.dir/node-v8.11.4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+    
+build_compile()
+{
+    PKG_CONFIG_PATH=/usr/lib/openssl-1.0/pkgconfig \
+    ./configure                \
+        --prefix=/usr          \
+        --without-npm          \
+        --with-intl=small-icu  \
+        --shared-cares         \
+        --shared-openssl       \
+        --shared-zlib
+
+    make
+}
+
+build_test_level=4
+build_test()
+{
+    make -j1 test
+}
+
+build_pack()
+{
+    make DESTDIR=$BUILD_PACK install
+
+    bandit_mkdir $BUILD_PACK/usr/share/doc
+    ln -sf node $BUILD_PACK/usr/share/doc/node-8.11.4
+}
