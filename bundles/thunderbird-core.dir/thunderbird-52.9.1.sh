5bcb6012b7b0d2467d69ef10ba3d5089abdfda26
Alz <alz@phyglos.org>
2018-09-11 13:43:48 +0200

    Upgraded to thunderbird-52.9.1

--- bundles/thunderbird-core.dir/thunderbird-52.9.1.sh
+++ bundles/thunderbird-core.dir/thunderbird-52.9.1.sh
@@ -15,42 +15,35 @@ build_compile()
 # If you have installed wireless-tools comment out this line:
 ac_add_options --disable-necko-wifi
 
-# GStreamer is necessary for H.264 video playback in HTML5 Video Player;
-# to be enabled, also remember to set "media.gstreamer.enabled" to "true"
-# in about:config. If you have GStreamer 1.x.y, comment out this line and
-# uncomment the following one:
-#ac_add_options --disable-gstreamer
-ac_add_options --enable-gstreamer=1.0
-
 # Uncomment these lines if you have installed optional dependencies:
 #ac_add_options --enable-system-hunspell
 ac_add_options --enable-startup-notification
 
 # Comment out following option if you have PulseAudio installed
 ac_add_options --disable-pulseaudio
+ac_add_options --enable-alsa
 
 # Comment out following option if you have gconf installed
-ac_add_options --disable-gconf
+#ac_add_options --disable-gconf
 
 # If you want to compile the Mozilla Calendar, uncomment this line:
 #ac_add_options --enable-calendar
 
 # Comment out following options if you have not installed
 # recommended dependencies:
-# Do not use system SQLite for Thunderbird 45.x
-# ac_add_options --enable-system-sqlite
-#ac_add_options --with-system-libevent
+ac_add_options --enable-system-sqlite
+ac_add_options --with-system-libevent
 ac_add_options --with-system-libvpx
 ac_add_options --with-system-nspr
 ac_add_options --with-system-nss
-ac_add_options --with-system-icu
+####ac_add_options --with-system-icu
 
 # The BLFS editors recommend not changing anything below this line:
 ac_add_options --prefix=/usr
 ac_add_options --enable-application=mail
 
 ac_add_options --disable-crashreporter
-ac_add_options --disable-updater
+#ac_add_options --disable-updater
 ac_add_options --disable-debug
 ac_add_options --disable-tests
 
@@ -63,9 +56,6 @@ ac_add_options --enable-official-branding
 ac_add_options --enable-safe-browsing
 ac_add_options --enable-url-classifier
 
-# Use internal cairo due to reports of unstable execution with
-# system cairo
-#ac_add_options --enable-system-cairo
 ac_add_options --enable-system-ffi
 ac_add_options --enable-system-pixman
 
@@ -90,10 +80,8 @@ build_pack()
 	 INSTALL_SDK=        \
 	 install
     
-#    chown -R 0:0 $BUILD_PACK/usr/lib/thunderbird-45.7.1
-
     bandit_mkdir $BUILD_PACK/usr/share/pixmaps
-    ln -sfv /usr/lib/thunderbird-45.7.1/chrome/icons/default/default256.png \
+    ln -sfv /usr/lib/thunderbird-52.9.1/chrome/icons/default/default256.png \
        $BUILD_PACK/usr/share/pixmaps/thunderbird.png
 
     bandit_mkdir $BUILD_PACK/usr/share/applications
