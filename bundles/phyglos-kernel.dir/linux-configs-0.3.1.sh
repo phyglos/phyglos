12b5b4cadc1e3fa2238f0d82393301768ac515c7
Alz <alz@phyglos.org>
2018-10-07 10:36:19 +0200

    Upgraded to linux-configs-0.3.1

--- bundles/phyglos-kernel.dir/linux-configs-0.3.1.sh
+++ bundles/phyglos-kernel.dir/linux-configs-0.3.1.sh
@@ -5,12 +5,18 @@ PHY_KERNEL_CFG=$PHY_KERNEL_SRC-$PHY_KERNEL_ARCH-$PHY_KERNEL_HW
 
 build_config()
 {
+    echo "Working with linux-configs/$PHY_KERNEL_CFG.config:" 
     if [ -e /boot/$PHY_KERNEL_CFG.config ]; then
-	echo "Using existing .config file from /boot directory..."
+	echo "Using existing .config file from /boot directory...skipping"
     else
+	echo "No .config file found in from /boot directory..."
 	if [ -e /var/lib/phyglos/linux-configs/$PHY_KERNEL_CFG.config ]; then
 	    echo "Copying the .config file from linux-configs package to the /boot directory..."
 	    cp -v /var/lib/phyglos/linux-configs/$PHY_KERNEL_CFG.config /boot/$PHY_KERNEL_CFG.config
+	else
+	    echo "Creating the /boot directory and copying the .config file..."
+	    bandit_mkdir /boot
+	    cp -v $PHY_KERNEL_CFG.config /boot/$PHY_KERNEL_CFG.config
 	fi
     fi  
 }
