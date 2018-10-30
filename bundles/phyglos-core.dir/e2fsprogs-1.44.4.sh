18d338ff321edeb01afc08ed18acd4ed9e3baa32
Alz <alz@phyglos.org>
2018-10-30 05:50:26 +0100

    Upgraded to e2fsprogs-1.44.4

--- bundles/phyglos-core.dir/e2fsprogs-1.44.4.sh
+++ bundles/phyglos-core.dir/e2fsprogs-1.44.4.sh
@@ -2,16 +2,9 @@
 
 build_compile()
 {
-    sed -e '/int.*old_desc_blocks/s/int/blk64_t/' \
-	-e '/if (old_desc_blocks/s/super->s_first_meta_bg/desc_blocks/' \
-	-i lib/ext2fs/closefs.c
-
     mkdir -v build
     cd build
 
-    LIBS=-L/$BANDIT_BUILDER_DIR/lib                    \
-    CFLAGS=-I/$BANDIT_BUILDER_DIR/include              \
-    PKG_CONFIG_PATH=/$BANDIT_BUILDER_DIR/lib/pkgconfig \
     ../configure                \
 	--prefix=/usr           \
         --bindir=/bin           \
@@ -28,9 +21,7 @@ build_compile()
 build_test_level=3
 build_test()
 {
-    ln -sfv /$BANDIT_BUILDER_DIR/lib/lib{blk,uu}id.so.1 lib
-
-    make LD_LIBRARY_PATH=/$BANDIT_BUILDER_DIR/lib check
+    make check
 }
 
 build_pack()
@@ -45,7 +36,7 @@ build_pack()
     install-info --dir-file=/usr/share/info/dir $BUILD_PACK/usr/share/info/com_err.info
 }
 
-install_unpack()
+install_setup()
 {
     yes | gunzip -v /usr/share/info/libext2fs.info.gz
     install-info --dir-file=/usr/share/info/dir /usr/share/info/libext2fs.info
