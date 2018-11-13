5182835336794d5dd9c385d6bd6673c6c90bf094
Alz <alz@phyglos.org>
2018-11-13 06:18:48 +0100

    Upgraded python3-core bundle to Python-3.7.1

--- bundles/python3-core.dir/Python-3.7.1-core.sh
+++ bundles/python3-core.dir/Python-3.7.1-core.sh
@@ -7,7 +7,6 @@ build_compile()
 	--prefix=/usr       \
 	--enable-shared     \
 	--with-system-expat \
-	--with-system-ffi   \
 	--without-ensurepip
     
     make
@@ -17,7 +16,7 @@ build_pack()
 {
     make DESTDIR=$BUILD_PACK install
 
-    chmod -v 755 $BUILD_PACK/usr/lib/libpython3.6m.so
+    chmod -v 755 $BUILD_PACK/usr/lib/libpython3.7m.so
     chmod -v 755 $BUILD_PACK/usr/lib/libpython3.so
 }
 
