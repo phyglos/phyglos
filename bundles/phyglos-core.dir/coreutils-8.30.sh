1c4c8d8686cc2ad7be1f0e290b7161af4031b9a3
Alz <alz@phyglos.org>
2019-07-15 06:05:11 +0200

    Upgraded to coreutils-8.30

--- bundles/phyglos-core.dir/coreutils-8.30.sh
+++ bundles/phyglos-core.dir/coreutils-8.30.sh
@@ -2,10 +2,11 @@
 
 build_compile()
 {
-    patch -Np1 -i $BUILD_SOURCES/coreutils-8.25-i18n-2.patch
+    patch -Np1 -i $BUILD_SOURCES/coreutils-8.30-i18n-1.patch
 
     sed -i '/tests\/misc\/sort.pl/ d' Makefile.in
 
+    autoreconf -fiv
     FORCE_UNSAFE_CONFIGURE=1 \
     ./configure              \
 	--prefix=/usr        \
@@ -32,8 +33,9 @@ build_pack()
 
     bandit_mkdir $BUILD_PACK/bin
     mv -v $BUILD_PACK/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} $BUILD_PACK/bin
-    mv -v $BUILD_PACK/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} $BUILD_PACK/bin
-    mv -v $BUILD_PACK/usr/bin/{rmdir,stty,sync,true,uname,test,[} $BUILD_PACK/bin
+    mv -v $BUILD_PACK/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm}        $BUILD_PACK/bin
+    mv -v $BUILD_PACK/usr/bin/{rmdir,stty,sync,true,uname,test,[}        $BUILD_PACK/bin
+    mv -v $BUILD_PACK/usr/bin/{head,sleep,nice}                          $BUILD_PACK/bin
 
     bandit_mkdir $BUILD_PACK/usr/sbin
     mv -v $BUILD_PACK/usr/bin/chroot $BUILD_PACK/usr/sbin
@@ -41,6 +43,4 @@ build_pack()
     bandit_mkdir $BUILD_PACK/usr/share/man/man8
     mv -v $BUILD_PACK/usr/share/man/man1/chroot.1 $BUILD_PACK/usr/share/man/man8/chroot.8
     sed -i s/\"1\"/\"8\"/1 $BUILD_PACK/usr/share/man/man8/chroot.8
-
-    mv -v $BUILD_PACK/usr/bin/{head,sleep,nice} $BUILD_PACK/bin
 }
