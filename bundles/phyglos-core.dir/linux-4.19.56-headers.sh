7a43f1ea4d85ee92ea04d12b143db31640d0c7a6
Alz <alz@phyglos.org>
2019-07-17 05:35:37 +0200

    Install kernel headers at build_pack() time

--- bundles/phyglos-core.dir/linux-4.19.56-headers.sh
+++ bundles/phyglos-core.dir/linux-4.19.56-headers.sh
@@ -3,13 +3,14 @@
 build_compile()
 {
     make mrproper
-
-    make INSTALL_HDR_PATH=dest headers_install
-    find dest/include \( -name .install -o -name ..install.cmd \) -delete
 }
        
 build_pack()
 {
+    make INSTALL_HDR_PATH=dest headers_install
+
+    find dest/include \( -name .install -o -name ..install.cmd \) -delete
+
     bandit_mkdir $BUILD_PACK/usr/include
     cp -rv dest/include/* $BUILD_PACK/usr/include
 }
