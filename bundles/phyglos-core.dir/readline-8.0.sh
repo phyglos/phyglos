4ef2987eb9d22ebe1819ea47c933c5cecd23c208
Alz <alz@phyglos.org>
2019-07-19 09:38:30 +0200

    Upgraded to readline-8.0

--- bundles/phyglos-core.dir/readline-8.0.sh
+++ bundles/phyglos-core.dir/readline-8.0.sh
@@ -2,27 +2,27 @@
 
 build_compile()
 {
-    patch -Np1 -i $BUILD_SOURCES/readline-6.3-upstream_fixes-3.patch
-
     sed -i '/MV.*old/d' Makefile.in
     sed -i '/{OLDSUFF}/c:' support/shlib-install
 
-    ./configure          \
-	--prefix=/usr    \
+    ./configure		 \
+	--prefix=/usr	 \
 	--disable-static \
-	--docdir=/usr/share/doc/readline-6.3 
+	--docdir=/usr/share/doc/readline-8.0
 
-    make SHLIB_LIBS=-lncurses
+    make SHLIB_LIBS="-L${BANDIT_BUILDER_DIR}/lib -lncurses"
 }
 
 build_pack()
 {
-    make DESTDIR=$BUILD_PACK SHLIB_LIBS=-lncurses install
+    make DESTDIR=$BUILD_PACK \
+	 SHLIB_LIBS="-L ${BANDIT_BUILDER_DIR}/lib -lncurses" \
+	 install
 
     bandit_mkdir $BUILD_PACK/lib
     mv -v $BUILD_PACK/usr/lib/lib{readline,history}.so.* $BUILD_PACK/lib
     ln -sfv ../../lib/$(readlink $BUILD_PACK/usr/lib/libreadline.so) $BUILD_PACK/usr/lib/libreadline.so
     ln -sfv ../../lib/$(readlink $BUILD_PACK/usr/lib/libhistory.so ) $BUILD_PACK/usr/lib/libhistory.so
 
-    install -v -m644 doc/*.{ps,pdf,html,dvi} $BUILD_PACK/usr/share/doc/readline-6.3
+    install -v -m644 doc/*.{ps,pdf,html,dvi} $BUILD_PACK/usr/share/doc/readline-8.0
 }
