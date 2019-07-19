b0cdf353cc57f16fb895800acb1b79a01c3a9a10
Alz <alz@phyglos.org>
2019-07-19 09:36:16 +0200

    Fixed missing link for libncurses.so

--- bundles/phyglos-builder.dir/ncurses-6.1-builder.sh
+++ bundles/phyglos-builder.dir/ncurses-6.1-builder.sh
@@ -5,12 +5,12 @@ build_compile()
     sed "s/mawk//" -i configure
 
     ./configure \
-        --prefix=$BANDIT_BUILDER_DIR \
-        --with-shared             \
-        --without-debug           \
-        --without-ada             \
-        --enable-widec            \
-        --enable-overwrite
+	--prefix=$BANDIT_BUILDER_DIR \
+	--with-shared		     \
+	--without-debug		     \
+	--without-ada		     \
+	--enable-widec		     \
+	--enable-overwrite
 
     make 
 }
@@ -18,4 +18,7 @@ build_compile()
 build_pack()
 {
     make DESTDIR=$BUILD_PACK install
+
+    bandit_mkdir $BUILD_PACK/$BANDIT_BUILDER_DIR/lib
+    ln -s libncursesw.so $BUILD_PACK/$BANDIT_BUILDER_DIR/lib/libncurses.so
 }
