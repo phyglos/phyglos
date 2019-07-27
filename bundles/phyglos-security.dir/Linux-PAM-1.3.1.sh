cb8a487e9aeb3234e9b3f583e5b0031adfd32bdd
Alz <alz@phyglos.org>
2019-07-27 05:09:23 +0200

    Upgraded to Linux-PAM-1.3.1

--- bundles/phyglos-security.dir/Linux-PAM-1.3.1.sh
+++ bundles/phyglos-security.dir/Linux-PAM-1.3.1.sh
@@ -2,12 +2,14 @@
 
 build_compile()
 {
+    tar -xf $BUILD_SOURCES/Linux-PAM-1.3.1-docs.tar.xz --strip-components=1
+    
     ./configure                           \
-	--prefix=/usr                     \
+        --prefix=/usr                     \
         --sysconfdir=/etc                 \
         --libdir=/usr/lib                 \
         --enable-securedir=/lib/security  \
-        --docdir=/usr/share/doc/Linux-PAM-1.2.1
+        --docdir=/usr/share/doc/Linux-PAM-1.3.1
 
     make
 }
@@ -16,20 +18,20 @@ build_test_level=1
 build_test()
 {
     if [ -d /etc/pam.d ]; then 
-	bandit_msg "PAM installed. Using actual /etc/pam.d for tests..."
-	make check
+        bandit_msg "PAM installed. Using actual /etc/pam.d for tests..."
+        make check
     else
-	bandit_msg "PAM not installed. Creating temporary /etc/pam.d for tests..."
-	install -v -m755 -d /etc/pam.d
-	cat > /etc/pam.d/other << "EOF"
+        bandit_msg "PAM not installed. Creating temporary /etc/pam.d for tests..."
+        install -v -m755 -d /etc/pam.d
+        cat > /etc/pam.d/other << EOF
 auth     required       pam_deny.so
 account  required       pam_deny.so
 password required       pam_deny.so
 session  required       pam_deny.so
 EOF
         make check
-	bandit_msg "Removing temporary /etc/pam.d..."
-	rm -rfv /etc/pam.d
+        bandit_msg "Removing temporary /etc/pam.d..."
+        rm -rfv /etc/pam.d
     fi
 }
 
@@ -41,25 +43,25 @@ build_pack()
 
     for file in pam pam_misc pamc
     do
-	mv -v $BUILD_PACK/usr/lib/lib${file}.so.* $BUILD_PACK/lib
-	ln -sfv ../../lib/$(readlink $BUILD_PACK/usr/lib/lib${file}.so) $BUILD_PACK/usr/lib/lib${file}.so
+        mv -v $BUILD_PACK/usr/lib/lib${file}.so.* $BUILD_PACK/lib
+        ln -sfv ../../lib/$(readlink $BUILD_PACK/usr/lib/lib${file}.so) $BUILD_PACK/usr/lib/lib${file}.so
     done
 
     bandit_mkdir $BUILD_PACK/etc/pam.d
-    cat > $BUILD_PACK/etc/pam.d/system-account << "EOF"
+    cat > $BUILD_PACK/etc/pam.d/system-account << EOF
 account   required    pam_unix.so
 EOF
 
-    cat > $BUILD_PACK/etc/pam.d/system-auth << "EOF"
+    cat > $BUILD_PACK/etc/pam.d/system-auth << EOF
 auth      required    pam_unix.so
 EOF
 
-    cat > $BUILD_PACK/etc/pam.d/system-session << "EOF"
+    cat > $BUILD_PACK/etc/pam.d/system-session << EOF
 session   required    pam_unix.so
 EOF
 
     # Password with Cracklib installed.
-    cat > $BUILD_PACK/etc/pam.d/system-password << "EOF"
+    cat > $BUILD_PACK/etc/pam.d/system-password << EOF
 # check new passwords for strength (man pam_cracklib)
 password  required    pam_cracklib.so   type=Linux retry=3 difok=5 \
                                         difignore=23 minlen=9 dcredit=1 \
@@ -71,7 +73,7 @@ password  required    pam_cracklib.so   type=Linux retry=3 difok=5 \
 password  required    pam_unix.so       sha512 shadow use_authtok
 EOF
     
-    cat > $BUILD_PACK/etc/pam.d/other << "EOF"
+    cat > $BUILD_PACK/etc/pam.d/other << EOF
 auth        required        pam_warn.so
 auth        required        pam_deny.so
 account     required        pam_warn.so
